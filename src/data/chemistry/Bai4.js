88dd93b383977c772c09fd10c683066ee8b01b50
linda <8597405+phucle2901@users.noreply.github.com>
2025-10-20 11:45:08 +0700

    10/20/2025

--- src/data/chemistry/Bai4.js
+++ src/data/chemistry/Bai4.js
@@ -89,7 +89,232 @@ export const chemistry = [
               <p>Vì hiệu suất đạt 95 % nên thực tế chỉ thu được :49 : 100 . 95 = 46.55 kg</p>
               `,
             },
-        //     {
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Người ta nung đá vôi (CaCO3) thu được vôi sống (CaO) và khí carbon dioxide. Lượng vôi sống thu được từ 1 tấn đá vôi có chứa 10% tạp chất là 0,45 tấn. Tính hiệu suất phản ứng.   </p>
+        `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `<p>Khối lượng CaCo3 tinh khiết: 1*90/100=0.9 tấn</p>
+              <p>Phương trình phản ứng:</p>
+              <p>CaCo3 <span class="inline-flex"><span class="congthuc font-bold "><span class="phanso"><span class="block leading-[5px]"><sup>t<sup>o</sup></sup></span><span class="mau leading-[5px]">→</span></span></span></span> CaO + CO2</p>
+              <p>100 g &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp  → 56 g</p>
+              <p>100 tấn &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp  → 56 tấn</p>
+              <p>0.9 Tấn &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp  → x tấn</p>
+              <p>Số tấn CaO theo lý thuyết thu được là x=0.9*56/100 =0,504 tấn</p>
+              <p>Hiệu suất phản ứng là: H=0.45/0.504*100%=89.29%</p>              
+              `,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Có thể điều chế bao nhiêu kg nhôm từ 1 tấn quặng nhôm có chứa 95% aluminium(III) oxide? Cho biết phương trình hóa học của phản ứng:</p><p>2Al2O3 <span class="inline-flex"><span class="congthuc font-bold "><span class="phanso"><span class="block leading-[5px]"><sup>xúc tác</sup></span><span class="mau leading-[5px]">→</span></span></span></span> 4Al + 3O2</p>
+              <p>Hiệu suất của phản ứng là 95%.</p>
+        `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `
+              <p>Khối lượng Al2O3 trong nguyên chất là: 1*95/100=0.95 tấn = 950 kg</p>
+              <p>Số mol Al2O3 tham gia phản ứng là nAl2O3 = 950/102=9.31 mol</p>
+              <p>Số mol  Al tạo thành theo lý thuyết là: 9.31 * 2 =18.62</p>
+              <p>Số mol Al thực tế là : 18.62*95/100 = 17.69 mol</p>
+              <p>Khối lượng Al thực tế là: 17.69*27=477.63 kg</p>
+              `,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Nung nóng KNO3, chất này bị phân hủy tạo thành KNO2 và O2. Biết hiệu suất phản ứng H = 80%. Tính khối lượng KNO3 cần dùng để điều chế 0,64 g O2. </p>
+        `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `
+              <p>Số mol O2 là 0,02 mol</p>
+              <p>Phương trình phản ứng:</p>
+              <p>2KNO3 <span class="inline-flex"><span class="congthuc font-bold "><span class="phanso"><span class="block leading-[5px]"><sup>xúc tác</sup></span><span class="mau leading-[5px]">→</span></span></span></span> 2KNO2 + O2</p>
+              <p>0.04 &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp 0.02 mol</p>
+              <p>Khối lượng KNO3 theo lý thuyết là: 0,04.101 = 4,04 g</p>
+              <p>Khối lượng KNO3 thực tế cần dùng là: 4,04*100/80 = 5,05 g</p>
+              
+              `,
+            },
+
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Đốt cháy 3,2 g lưu huỳnh trong không khí thu được V lít khí SO2 ở đktc, biết hiệu suất phản ứng là 80%. Tính V.</p>
+        `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `
+              <p>Ta có nS = 0,1 mol</p>
+              <p>Phương trình phản ứng:</p>
+              <p>S + O2 <span class="inline-flex"><span class="congthuc font-bold "><span class="phanso"><span class="block leading-[5px]"><sup>t<sup>o</sup></sup></span><span class="mau leading-[5px]">→</span></span></span></span> SO2</p>
+              <p>0.1 &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp 0.1 mol</p>
+              <p>Vì H = 80%, suy ra số mol của SO2 là  0,1.80% = 0,08 mol</p>
+              <p>Vậy thể tích của SO2 ở đktc là: V = 0,08.22,4 = 1,792 lít.</p>
+              `,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Khử 24 g CuO bằng khí hiđro ở nhiệt độ cao. Biết rắng hiệu suất phản ứng H = 80%. Tính Số gam kim loại đồng thu được.</p>
+        `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `
+              <p>Ta có nCuO = 0,3 mol</p>
+              <p>Phương trình phản ứng:</p>
+              <p>CuO + H2 <span class="inline-flex"><span class="congthuc font-bold "><span class="phanso"><span class="block leading-[5px]"><sup>t<sup>o</sup></sup></span><span class="mau leading-[5px]">→</span></span></span></span> Cu + H2O</p>
+              <p>0.3  &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp 0.3 mol</p>
+              <p>Suy ra mCu = 0,3.64 = 19,2 g</p>
+              <p>Vì H = 80% nên khối lượng Cu thu được là m = 19,2*80/100 = 15,36 g</p>
+              `,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p> Nung 1 kg đá vôi chứa 80% CaCO3 thu đƣợc 123.95 dm3 CO2 (đkc) .Tính hiệu suất phân huỷ CaCO3.</p>
+        `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `<p></p>
+              <p>1 kg = 1000 g</p>
+              <p>123.95 dm3 = 123.95 lít</p>
+              <p>n<sub>CO2</sub>= 123.95 / 24.79 =5 (mol)</p>
+              <p>m<sub>CaCO3</sub> = 1000*80/100 = 800 (g)</p>
+              <p>n<sub>CaCO3</sub>=800/100 = 8 (mol)</p>
+              <p>PTHH: CaCo3 <span class="inline-flex"><span class="congthuc font-bold "><span class="phanso"><span class="block leading-[5px]"><sup>t<sup>o</sup></sup></span><span class="mau leading-[5px]">→</span></span></span></span> CaO + CO2</p>
+              <p>&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp  1 mol &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp 1 mol</p>
+              <p>&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp  8 mol &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp 8 mol</p>
+              <p>Theo lý thuyết thì 8 mol CaCo3 tạo ra  8 mol CO2</p>
+              <p>Thực thế chỉ có 5 mol CO2 được tạo ra</p>
+              <p>Hiệu suất phân huỷ CaCO3 là: H = 5/8 *100% = 62.5%</p>
+              `,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Khi cho khí SO3 hợp nước cho ta dung dịch H2SO4. Tính lựợng H2SO4 điều chế 
+đựợc khi cho 40 Kg SO3 hợp nước. Biết Hiệu suất phản ứng là 95%.</p>
+        `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `
+              <p>Phương trình phản ứng:</p>
+              <p>SO3 + H2O -> H2SO4</p>
+              <p>80 g &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp 98 g</p>
+              <p>40 kg  &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp x kg/p>
+              <p>Số kg H2SO4 thu được theo lý thuyết là: 40*98/80 =49 kg</p>
+              <p>Vì hiệu suất đạt 95 % nên thực tế chỉ thu được : 49 * 95 /100= 46.55 kg.</p>
+              `,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p> Người ta dùng quặng boxit để sản xuất nhôm theo sơ đồ phản ứng sau: 
+Al2O3  <span class="inline-flex"><span class="congthuc font-bold "><span class="phanso"><span class="block leading-[5px]"><sup>t<sup>o</sup></sup></span><span class="mau leading-[5px]">→</span></span></span></span>   Al + O2</p>
+<p>Hàm lượng Al2O3 trong quặng boxit là 40% . Để có đƣợc 4 tấn nhôm nguyên chất 
+cần bao nhiêu tấn quặng. Biết H của quá trình sản xuất là 90%</p>
+        `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `
+              <p>Phương trình phản ứng:</p>
+              <p>Số Tấn Al lý thuyết cần là: 4*100/90= 4.44 Tấn</p>
+              <p>2Al2O3  <span class="inline-flex"><span class="congthuc font-bold "><span class="phanso"><span class="block leading-[5px]"><sup>t<sup>o</sup></sup></span><span class="mau leading-[5px]">→</span></span></span></span>   4Al + 3O2</p>
+              <p>204 g &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp 108 g</p>
+
+              <p>Số tấn Al2O3 lý thuyết cần để là: 204*44,4/108=8.39 Tấn </p>
+              <p>Vì Al2O3 chiếp 40% trong quặng boxit, nên số tấn quặng cần là: 8.39*100/40=20.96 Tấn</p>
+              <p></p>
+              `,
+            },
+            
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Người ta dùng 490kg than để đốt lò chạy máy. Sau khi lò nguội, thấy còn 49kg than chưa cháy. </p>
+              <p>a) Tính hiệu suất của sự cháy trên.</p>
+              <p>Tính lượng CaCO3 thu được, khi cho toàn bộ khí CO2 vào nƣớc vôi trong dư. </p>
+        `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `
+              <p>a)</p>
+              <p>PTHH: 2C + O2 <span class="inline-flex"><span class="congthuc font-bold "><span class="phanso"><span class="block leading-[5px]"><sup>t<sup>o</sup></sup></span><span class="mau leading-[5px]">→</span></span></span></span> CO2 (1)</p>
+              <p>m<sub>C pứ</sub> = 490 -49 = 441 kg</p>
+              <p>H = 441*100/490 = 90 %</p>
+              <p>n<sub>C</sub>= 441/12=36.75 mol</p>
+              <p>n<sub>CO2</sub> = 36.75 mol</p>
+              <p>b)</p>
+              <p>PTHH: CO2 + Ca(OH)2  -> CaCO3 + H2O (2)</p>
+              <p>&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp 1 mol &nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp 1 mol</p>
+              <p>&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp 36.75 mol &nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp 36.75 mol</p>
+              <p>m<sub>CaCO3</sub> = 36.75*100 =3675 kg</p>
+              <p></p>
+              `,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Đốt cháy 6,5 g lưu huỳnh không tinh khiết trong khí oxi dư được 4.958l khí SO2 ở đkc </p>
+              <p>a) Viết PTHH xảy ra. </p>
+              <p>b) Tính độ tinh khiết của mẫu lưu huỳnh trên? </p>
+        `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `
+              <p>a) S + O2 <span class="inline-flex"><span class="congthuc font-bold "><span class="phanso"><span class="block leading-[5px]"><sup>t<sup>o</sup></sup></span><span class="mau leading-[5px]">→</span></span></span></span> SO2</p>
+              <p>n<sub>SO2</sub> = 4.958 / 24.79 = 0.2 (mol)</p>
+              <p>b) Theo PTHH: 1 mol S tạo ra 1 mol SO2</p>
+              <p>n<sub>S pứ</sub> = 0.2 mol</p>
+              <p>m<sub>S pứ</sub> = 0.2 * 32 = 6.4 g</p>
+              <p>Độ tinh khiết của mẫu lưu huỳnh là: 6.4 * 100 / 6.5 = 98.46 %</p>   
+              <p>Ghi chú: Độ tinh khiết = 100% - % tạp chất </p>   
+              <p>Hoặc độ tinh khiết = khối lƣợng chất tinh khiết*100% /Khối lƣợng ko tinh khiết</p>        
+              `,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Ở 1 nông trường nta dùng CuSO4.5H2O để bón ruộng . Người ta bón 25kg muối trên vào đất. lượng Cu được đưa vào đát là bao nhiêu? Biết CuSO4.5H2O chưa 5% tạp chất</p>
+        `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `
+              <p>m<sub>CuSO4.5H2O nguyên chất</sub> = 25 * (100 - 5) / 100 =23.75(kg) = 23750 g</p>
+              <p>Ta có: </p>
+              <p>64 g Cu  ----------------> 250 g CuSO4.5H2O</p>
+              <p>x g Cu  ----------------> 23750 g CuSO4.5H2O</p>
+              <p>Suy ra x= 23750*64/250 =6080 (g)</p>
+              <p>Vậy khi bón 25kg muối CuSO4.5H2O vào đất thì có 6,08kg Cu được đưa vào đất.</p>
+              `,
+            },
+         //     {
         //       id: 41,
         //       extra: ``,
         //       question: `
@@ -100,7 +325,7 @@ export const chemistry = [
         //       lastwrite: ``,
         //       explanation: ``,
         //     },
-        //     {
+         //     {
         //       id: 41,
         //       extra: ``,
         //       question: `
@@ -111,29 +336,7 @@ export const chemistry = [
         //       lastwrite: ``,
         //       explanation: ``,
         //     },
-        //     {
-        //       id: 41,
-        //       extra: ``,
-        //       question: `
-        // `,
-        //       options: [],
-        //       answer: ``,
-        //       rewrite: ``,
-        //       lastwrite: ``,
-        //       explanation: ``,
-        //     },
-        //     {
-        //       id: 41,
-        //       extra: ``,
-        //       question: `
-        // `,
-        //       options: [],
-        //       answer: ``,
-        //       rewrite: ``,
-        //       lastwrite: ``,
-        //       explanation: ``,
-        //     },
-        //     {
+         //     {
         //       id: 41,
         //       extra: ``,
         //       question: `

ebeb6f06e3d57eab0e5fb864615534d893ca7916
linda <8597405+phucle2901@users.noreply.github.com>
2025-10-31 15:49:28 +0700

    10/31/2025 2

--- src/data/chemistry/Bai1.js
+++ src/data/chemistry/Bai1.js
@@ -1,118 +1,118 @@
 export const chemistry = [
-    {      
-      semester: 1,
-      grade: 8,
-      questions: [
-          {
-              id: 41,
-              extra: ``,
-              question: `
-        `,
-              options: [],
-              answer: ``,
-              rewrite: ``,
-              lastwrite: ``,
-              explanation: ``,
-            },
-            {
-              id: 41,
-              extra: ``,
-              question: `
-        `,
-              options: [],
-              answer: ``,
-              rewrite: ``,
-              lastwrite: ``,
-              explanation: ``,
-            },
-            {
-              id: 41,
-              extra: ``,
-              question: `
-        `,
-              options: [],
-              answer: ``,
-              rewrite: ``,
-              lastwrite: ``,
-              explanation: ``,
-            },
-            {
-              id: 41,
-              extra: ``,
-              question: `
-        `,
-              options: [],
-              answer: ``,
-              rewrite: ``,
-              lastwrite: ``,
-              explanation: ``,
-            },
-            {
-              id: 41,
-              extra: ``,
-              question: `
-        `,
-              options: [],
-              answer: ``,
-              rewrite: ``,
-              lastwrite: ``,
-              explanation: ``,
-            },
-            {
-              id: 41,
-              extra: ``,
-              question: `
-        `,
-              options: [],
-              answer: ``,
-              rewrite: ``,
-              lastwrite: ``,
-              explanation: ``,
-            },
-            {
-              id: 41,
-              extra: ``,
-              question: `
-        `,
-              options: [],
-              answer: ``,
-              rewrite: ``,
-              lastwrite: ``,
-              explanation: ``,
-            },
-            {
-              id: 41,
-              extra: ``,
-              question: `
-        `,
-              options: [],
-              answer: ``,
-              rewrite: ``,
-              lastwrite: ``,
-              explanation: ``,
-            },
-            {
-              id: 41,
-              extra: ``,
-              question: `
-        `,
-              options: [],
-              answer: ``,
-              rewrite: ``,
-              lastwrite: ``,
-              explanation: ``,
-            },
-            {
-              id: 41,
-              extra: ``,
-              question: `
-        `,
-              options: [],
-              answer: ``,
-              rewrite: ``,
-              lastwrite: ``,
-              explanation: ``,
-            },
-      ]
-  },
+  //   {      
+  //     semester: 1,
+  //     grade: 8,
+  //     questions: [
+  //         {
+  //             id: 41,
+  //             extra: ``,
+  //             question: `
+  //       `,
+  //             options: [],
+  //             answer: ``,
+  //             rewrite: ``,
+  //             lastwrite: ``,
+  //             explanation: ``,
+  //           },
+  //           {
+  //             id: 41,
+  //             extra: ``,
+  //             question: `
+  //       `,
+  //             options: [],
+  //             answer: ``,
+  //             rewrite: ``,
+  //             lastwrite: ``,
+  //             explanation: ``,
+  //           },
+  //           {
+  //             id: 41,
+  //             extra: ``,
+  //             question: `
+  //       `,
+  //             options: [],
+  //             answer: ``,
+  //             rewrite: ``,
+  //             lastwrite: ``,
+  //             explanation: ``,
+  //           },
+  //           {
+  //             id: 41,
+  //             extra: ``,
+  //             question: `
+  //       `,
+  //             options: [],
+  //             answer: ``,
+  //             rewrite: ``,
+  //             lastwrite: ``,
+  //             explanation: ``,
+  //           },
+  //           {
+  //             id: 41,
+  //             extra: ``,
+  //             question: `
+  //       `,
+  //             options: [],
+  //             answer: ``,
+  //             rewrite: ``,
+  //             lastwrite: ``,
+  //             explanation: ``,
+  //           },
+  //           {
+  //             id: 41,
+  //             extra: ``,
+  //             question: `
+  //       `,
+  //             options: [],
+  //             answer: ``,
+  //             rewrite: ``,
+  //             lastwrite: ``,
+  //             explanation: ``,
+  //           },
+  //           {
+  //             id: 41,
+  //             extra: ``,
+  //             question: `
+  //       `,
+  //             options: [],
+  //             answer: ``,
+  //             rewrite: ``,
+  //             lastwrite: ``,
+  //             explanation: ``,
+  //           },
+  //           {
+  //             id: 41,
+  //             extra: ``,
+  //             question: `
+  //       `,
+  //             options: [],
+  //             answer: ``,
+  //             rewrite: ``,
+  //             lastwrite: ``,
+  //             explanation: ``,
+  //           },
+  //           {
+  //             id: 41,
+  //             extra: ``,
+  //             question: `
+  //       `,
+  //             options: [],
+  //             answer: ``,
+  //             rewrite: ``,
+  //             lastwrite: ``,
+  //             explanation: ``,
+  //           },
+  //           {
+  //             id: 41,
+  //             extra: ``,
+  //             question: `
+  //       `,
+  //             options: [],
+  //             answer: ``,
+  //             rewrite: ``,
+  //             lastwrite: ``,
+  //             explanation: ``,
+  //           },
+  //     ]
+  // },
 ]
--- src/data/chemistry/Bai5.js
+++ src/data/chemistry/Bai5.js
@@ -169,6 +169,213 @@ export const chemistry = [
                Giải phương trình được x = 25.
               </p>`,
             },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Hoà tan 25,5 gam NaCl vào 80 gam nƣớc ở 200C được dung dịch A. Hỏi dung 
+dịch A đã bão hòa hay chưa? Biết độ tan của NaCl ở 200C là 38 gam. </p>
+              `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `<p>Độ tan của NaCl ở 20 độ C là 38 gam, nghĩa là:<br/>
+              100 gam H2O hòa tan được 38 gam NaCl <br/>
+              Suy ra với 80 gam H2O sẽ hòa tan được : 38 x 80/100 = 30,4 gam NaCl <br/>
+              Vì 25,5 gam NaCl < 30,4 gam NaCl nên dung dịch A chưa bão hòa, phải cần  thêm 30,4 - 25,5 = 4,9 gam NaCl mới được dung dịch bão hòa
+              </p>`,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Khi làm lạnh 600 gam dung dịch bão hòa NaCl từ 900C xuống 100C thì có bao nhiêu gam muối NaCl tách ra. Biết rằng độ tan của NaCl ở 900C là 50 gam và ở 100C là 35 gam.</p>
+              `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `<p>
+              Ở 90°C, độ tan của NaCl là 50 gam nghĩa là có 50 gam NaCl tan trong 100 gam H2O tạo 150 gam dung dịch NaCl bão hòa.<br/>
+              Như vậy 600 gam dung dịch NaCl bão hòa ở 90°C có:<br/>
+              m<sub>H2O</sub> = (100/150) x 600 = 400 gam<br/>
+              m<sub>NaCl</sub> = 600 – 400 = 200 gam<br/>
+              Ở 10°C, độ tan của NaCl là 35 gam nghĩa là có 35 gam NaCl tan trong 100 gam H2O tạo 135 gam dung dịch NaCl bão hòa.<br/>
+              Với 400 gam H2O chỉ hòa tan được:<br/>
+              m<sub>NaCl</sub> = (35/100) x 400 = 140 gam<br/>
+              Vậy khối lượng NaCl tách ra khi làm lạnh dung dịch là:<br/>
+              200 – 140 = 60 gam
+              </p>`,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Một dung dịch có chứa 26,5 gam NaCl trong 75 gam H2O ở 200C. Hãy xác định 
+lượng dung dịch NaCl nói trên là bão hòa hay chưa bão hòa? Biết rằng độ tan 
+của NaCl trong nước ở 200C là 36 gam. </p>
+              `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `<p>
+              Cứ 100 gam H2O hòa tan được 36 gam NaCl <br/>
+              mdd= 100+ 36= 136g<br/>
+              Suy ra với 75 gam H2O sẽ hòa tan được : 36 x 75/100 = 27 gam NaCl <br/>
+              Vì 26,5 gam NaCl < 27 gam NaCl nên dung dịch chưa bão hòa.              
+              </p>`,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Hòa tan 7,18 gam muối NaCl vào 20 gam nước ở 200C thì được dung dịch bão 
+hòa. Độ tan của NaCl ở nhiệt độ đó là bao nhiêu?</p>
+              `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `<p>
+              C1:<br/>
+              Gọi x là độ tan của NaCl ở 200C<br/>
+              Cứ 100 gam H2O hòa tan được x gam NaCl <br/>
+              Hòa tan 7.18 gam NaCl vào 20 gam nước thì được dung dịch bão hòa <br/>
+              Suy ra với 20 gam H2O sẽ hòa tan được : x x 20/100 = 7.18 gam NaCl <br/>
+              Giải phương trình ta được x = 35.9 gam <br/>
+              Vậy độ tan của NaCl ở 200C là 35.9 gam<br/>
+              C2:<br/>
+
+              Ta có S=(m<sub>ct</sub>/m<sub>dd</sub>)*100=7.18*100/20=35.9(g)<br/>
+              100 gam H2O hòa tan được 35.9 gam NaCl<br/>
+              </p>`,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>a) Hòa tan 4 gam NaCl trong 80 gam H2O. Tính nồng độ phần trăm của dung dịch</p>
+              <p>b) Chuyển sang nồng độ phần trăm dung dịch NaOH 2M có khối lượng    riêng d = 1,08 g/ml.</p>
+              <p> Cần bao nhiêu gam NaOH để pha chế được 3 lít dung dịch NaOH 10%. Biết khối 
+lượng riêng của dung dịch là 1,115 g/ml.</p>
+              `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `<p>
+              a) Tính nồng độ phần trăm của dung dịch NaCl<br/>
+              m<sub>dd</sub> = m<sub>ct</sub> + m<sub>H2O</sub> = 4 + 80 = 84 gam<br/>
+              C% = (m<sub>ct</sub>/m<sub>dd</sub>)*100% = (4/84)*100% = 4,76%<br/>
+              b) Xem V<sub>dd</sub>=100ml<br/>
+              n<sub>NaOH</sub> = C<sub>M</sub> x V = 2 x 0,1 = 0,2 mol<br/>
+              m<sub>NaOH</sub> = n x M = 0,2 x 40 = 8 gam<br/>
+              C% = (m<sub>ct</sub>/m<sub>dd</sub>)*100% = (8/(108))*100% = 7,41%<br/>
+              c)<br>
+              Ta có: V<sub>dd</sub> = 3 lít = 3000 ml<br/>
+              m<sub>dd</sub> = d x V = 1,115 x 3000 = 3345 gam<br/>
+              m<sub>ct</sub> = C% x m<sub>dd</sub>/100% = 10% x 3345/100% = 334,5 gam<br/>
+
+              </p>`,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Dung dịch H2SO4 có nồng độ 0,2 M (dung dịch A). Dung dịch H2SO4 có nồng độ 
+0,5M (dung dịch B).</p>
+<p>a) Nếu trộn A và B theo tỷ lệ thể tích VA: VB  =  2 : 3 đƣợc dung dịch C. Hãy 
+xác định nồng độ mol của dung dịch C. </p>
+<p>b) Phải trộn A và B theo tỷ lệ nào về thể tích để được dung dịch H2SO4 có 
+nồng độ 0,3 M.</p>
+              `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `<p>
+              a)C1:<br/>
+              Theo đề bài ta có:<br/>
+              V<sub></sub>A</sub> / V<sub>B</sub> =2/3<br/>
+              Ta có V<sub>A</sub> = V<sub>B</sub>*2/3<br/>
+              n<sub>H2SO4 A</sub> = C<sub>M A</sub> x V<sub>A</sub> = 0,2 x V<sub>B</sub>*2/3 <br/>
+              n<sub>H2SO4 B</sub> = C<sub>M B</sub> x V<sub>B</sub> = 0,5 x V<sub>B</sub><br/>
+              n<sub>H2SO4 C</sub> = n<sub>H2SO4 A</sub> + n<sub>H2SO4 B</sub> = 0,2 x V<sub>B</sub>*2/3 + 0,5 x V<sub>B</sub> = 0,6333 V<sub>B</sub><br/>
+              C<sub>M C</sub> = n<sub>H2SO4 C</sub> / V<sub>C</sub> = 0,6333 V<sub>B</sub> / (V<sub>A</sub> + V<sub>B</sub>) = 0,6333 V<sub>B</sub> / (V<sub>B</sub>*2/3 + V<sub>B</sub>) = 0,38 M<br/>
+              C2:<br/>
+              Sử dụng công thức pha trộn dung dịch:<br/>              
+              </p>
+              <table>
+              <tr><td>V<sub>A</sub> ml dung dịch C<sub>MA</sub>  
+              </td><td></td><td>|C<sub>MB</sub> - C<sub>MC</sub>|</td><td></td><td>V<sub>A</sub></td><td></td><td> |C<sub>MB</sub> - C<sub>MC</sub>|</td></tr>
+              <tr><td></td><td> C<sub>MC</sub></td><td></td><td> -> </td><td>--</td><td> = </td><td> ------------ </td></tr>
+              <tr></tr><td>V<sub>B</sub> ml dung dịch C<sub>MB</sub></td><td></td><td>|C<sub>MA</sub> - C<sub>MC</sub> |</td><td></td><td>V<sub>B</sub></td><td></td><td>|C<sub>MA</sub> - C<sub>MC</sub>|</td></tr>
+              </table>
+              <p>Vì C<sub>MA</sub> < C<sub>MB</sub> nên:</p>
+              <table>
+              <tr><td>V<sub>A</sub></td><td></td><td>C<sub>MB</sub> - C<sub>MC</sub></td><td></td><td>2</td><td></td><td> 0.5 - C<sub>MC</sub></td></tr>
+              <tr><td>--</td><td> = </td><td>------------</td><td> = </td><td> -- </td><td> = </td><td> ------------- </td></tr>
+              <tr><td>V<sub>B</sub></td><td></td><td>C<sub>MC</sub> - C<sub>MA</sub></td><td></td><td>3</td><td></td><td> C<sub>MC</sub> - 0.2</td></tr>
+              </table>
+              <p>2(C<sub>MC</sub> - 0.2) =3(0.5 - C<sub>MC</sub>)</p>
+              <p>=> C<sub>MC</sub> = 0,38 M</p>
+              <p>b)<br/>
+              Gọi VA là thể tích dung dịch A cần lấy, VB là thể tích dung dịch B cần lấy.<br/>
+              C<sub>MC</sub> = (C<sub>MA</sub> x V<sub>A</sub> + C<sub>MB</sub> x V<sub>B</sub>) / (V<sub>A</sub> + V<sub>B</sub>)<br/>
+              0,3 = (0,2 x V<sub>A</sub> + 0,5 x V<sub>B</sub>) / (V<sub>A</sub> + V<sub>B</sub>)<br/>
+              0,3V<sub>A</sub> + 0,3V<sub>B</sub> = 0,2V<sub>A</sub> + 0,5V<sub>B</sub><br/>
+              0,1V<sub>A</sub> = 0,2V<sub>B</sub><br/>
+              V<sub>A</sub> / V<sub>B</sub> = 2/1<br/>
+
+              </p>
+              `,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Phải lấy bao nhiêu ml dung dịch H2SO4 96% (d = 1,84 g/ml) để trong đó có chứa 2,45g H2SO4.</p>
+              `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `<p>
+              Khối lượng dung dịch:<br/>
+              m<sub>dd</sub> = m<sub>ct</sub> x 100% / C% = 2,45 x 100% / 96% = 2,55 g<br/>
+              Thể tích dung dịch cần lấy:<br/>
+              V= m/d = 2,55/1,84 = 1,39 ml
+              </p>`,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `<p>Oxi hóa hoàn toàn 6.2 lít khí SO2 (đkc) vào trong 57,2 ml dung dịch H2SO4 
+60% (D =1,5 g/ml). Tính nồng độ % của dung dịch axit thu được</p>
+              `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: `<p><img src="/learn/hoa/Baitap5_18.png"/></p>`,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `
+              `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: ``,
+            },
+            {
+              id: 41,
+              extra: ``,
+              question: `
+              `,
+              options: [],
+              answer: ``,
+              rewrite: ``,
+              lastwrite: ``,
+              explanation: ``,
+            },
       ]
   },
 ]

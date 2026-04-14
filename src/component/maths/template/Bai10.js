dff20810d3804ec88a0f0550fba7fb993037597f
linda <8597405+phucle2901@users.noreply.github.com>
2026-04-14 13:36:47 +0700

    14/04/2026

---
+++ src/component/maths/template/Bai10.js
@@ -0,0 +1,38 @@
+import React from 'react';
+const Bai10 = () => {
+    return (
+        <div class="leading-[35px]" style={{
+            padding: "0.8em",
+            border: "1px solid #ccc",
+            borderRadius: "5px",
+            background: "#ffffff",
+            marginBottom: "1em",            
+          }}>
+            <h1 class="text-center text-[1.7em] mb-5 text-shadow-h1">
+          <b>Chuyên đề: Định lý Talet trong tam giác</b>
+        </h1>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_1.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_2.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_3.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_4.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_5.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_6.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_7.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_8.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_9.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_10.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_11.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_12.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_13.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_14.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_15.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_16.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_17.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_18.png'}/></p>
+        <p><img alt='1' src={process.env.PUBLIC_URL + '/toan/10/1_19.png'}/></p>
+        
+        </div>
+    );
+};
+
+export default Bai10;
